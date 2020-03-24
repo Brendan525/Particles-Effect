9a3ec16f228eca4c398c8b4fc017cfd853cd4e1c
Brendan525 <broe5252@hotmail.com>
2020-03-24 09:18:55 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,78 @@
+const particles = [];
+
+function setup() {
+    createCanvas(window.innerWidth, window.innerHeight);
+
+    const particlesLength = Math.floor(window.innerWidth / 10);
+
+    // console.log(particlesLength)
+
+    for(let i = 0; i < particlesLength; i++) {
+        particles.push(new Particle());
+    }
+}
+
+function draw() {
+    background(55, 100, 144);
+    particles.forEach((p, index) => {
+        p.update();
+        p.draw();
+        p.checkParticles(particles.slice(index)); // Calls function (checkParticles to creates the lines
+    })
+}
+
+class Particle {
+    constructor() {
+        // Position
+        this.pos = createVector(random(width), random(height));
+        // Velocity
+        this.vel = createVector(random(-2, 2), random(-2,2)); // To change speed change numbers in random
+        // Size
+        this.size = 10;
+    }
+
+    // Update movement by adding velocity
+    update() {
+        this.pos.add(this.vel);
+        this.edges();
+    }
+
+    // Draw single particle
+    draw() {
+        noStroke();
+        fill('rgba(255,255,255,0.5)');
+        circle(this.pos.x, this.pos.y, this.size);
+    }
+
+    // Detect edges
+    edges() {
+        if(this.pos.x < 0 || this.pos.x > width) {
+            this.vel.x *= -1;
+        }
+
+        if(this.pos.y < 0 || this.pos.y > height) {
+            this.vel.y *= -1;
+        }
+    }
+
+    // Connect particles
+    checkParticles(particles) {
+        particles.forEach(particle => {
+            const d = dist(this.pos.x, this.pos.y, particle.pos.x, particle.pos.y);
+
+            if(d < 120) {
+                stroke('rgba(255,255,255,0.1)')
+                line(this.pos.x, this.pos.y, particle.pos.x, particle.pos.y)
+            }
+        });
+    }
+}
+
+
+ //     if(mouseIsPressed) {
+//     fill(0);
+//     } else {
+//     fill(100);
+// }
+
+//     circle(mouseX, mouseY, 80);
